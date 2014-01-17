81b792535fb0ebebadaf3da1150ee158110f2bd0
Gopal-Selvaraj <gopal.selvaraj@a-cti.com>
2014-01-17 20:45:24 +0530

    Address

---
+++ src/collections/Address.java
@@ -0,0 +1,31 @@
+package collections;
+
+public class Address {
+	private String district;
+	private String state;
+	private String country;
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
